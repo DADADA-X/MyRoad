9ed7f339ae43ce91fb56e1378a5b7b4c5fa4466f
DADADA-X <35071062+DADADA-X@users.noreply.github.com>
2020-07-28 14:17:39 +0800

    Initial commit

---
+++ train.py
@@ -0,0 +1,68 @@
+import argparse
+import collections
+import torch
+import numpy as np
+import data_loader.data_loaders as module_data
+import model.loss as module_loss
+import model.metric as module_metric
+import model.model as module_arch
+from parse_config import ConfigParser
+from trainer import Trainer
+
+# fix random seeds for reproducibility
+SEED = 123
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+
+
+def main(config):
+    logger = config.get_logger('train')     # TODO 这里有一个verbosity的参数，设置的是logging级别，有012三个值，对应 warning, info, debug(default)
+
+    # setup data_loader instances
+    data_loader = config.init_obj('data_loader', module_data)
+    valid_data_loader = data_loader.split_validation()
+
+    # build model architecture, then print to console
+    model = config.init_obj('arch', module_arch)
+    logger.info(model)  # 设置日志级别，会打印model，有名字，结构，参数个数等信息
+
+    # get function handles of loss and metrics
+    criterion = getattr(module_loss, config['loss'])
+    metrics = [getattr(module_metric, met) for met in config['metrics']]
+
+    # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
+    trainable_params = filter(lambda p: p.requires_grad, model.parameters())    # 这个tensor在前向传播过程中，就会生成一个计算图，便于进行backpropagation
+    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+
+    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+
+    trainer = Trainer(model, criterion, metrics, optimizer,
+                      config=config,
+                      data_loader=data_loader,
+                      valid_data_loader=valid_data_loader,
+                      lr_scheduler=lr_scheduler)
+
+    trainer.train()
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')  # 实例化对象
+    args.add_argument('-c', '--config', default=None, type=str,  # 添加参数
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    # custom cli options to modify configuration from default values given in json file.
+    # 自定义命令行选项，用来修正json文件中的默认配置
+    CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')  # flag 标识
+    # TODO 这里是可以改的，会在后续添加命令行参数。打开config.json，在这里添加要修改的参数，不要在原文件中改 TODO #
+    options = [
+        CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
+        CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size')
+    ]
+    config = ConfigParser.from_args(args, options)  # options最后还是会以add_argument的形式加到args里面去
+    main(config)
